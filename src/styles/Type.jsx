674f89c53c572cd4a71fb4e5ecca6bee560ee604
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-02 16:50:21 -0600

    remove start files + global utils

---
+++ src/styles/Type.jsx
@@ -0,0 +1,72 @@
+import { Variables } from './Variables';
+import { MediaQueries } from './Utilities';
+import { css } from 'styled-components';
+
+export const H1Styles = css`
+    font-family: ${Variables.openSans};
+    font-size: 8rem;
+    line-height: 8.3rem;
+    color: ${Variables.white};
+
+    @media ${MediaQueries.tablet} {
+        font-size: 7rem;
+        line-height: 7.3rem;
+    }
+
+    @media ${MediaQueries.mobile} {
+        font-size: 4.2rem;
+        line-height: 4.4rem;
+    }
+`;
+
+export const H2Styles = css`
+    font-family: ${Variables.joseph};
+    font-size: 6rem;
+
+    @media ${Variables.tablet} {
+        font-size: 4.5rem;
+    }
+
+    @media ${Variables.mobile} {
+        font-size: 3.5rem;
+    }
+`;
+
+export const H3Styles = css`
+    font-family: ${Variables.joseph};
+    font-size: 3rem;
+`;
+
+export const H4Styles = css`
+    font-family: ${Variables.joseph};
+    font-size: 2em;
+`;
+
+export const PBaseStyles = css`
+    font-family: ${Variables.joseph};
+    font-size: 3rem;
+    line-height: 3.3rem;
+    color: ${Variables.white};
+
+    @media ${MediaQueries.tablet} {
+        font-size: 2.5rem;
+        line-height: 2.8rem;
+    }
+
+    @media ${MediaQueries.mobile} {
+        font-size: 1.8rem;
+        line-height: 2rem;
+    }
+`;
+
+export const PSecondary = css`
+    font-family: ${Variables.joseph};
+    font-size: 1.75rem;
+    line-height: 25px;
+`;
+
+export const PSmallStyles = css`
+    font-family: ${Variables.joseph};
+    font-size: 1.2rem;
+    line-height: 1.5rem;
+`;
---
+++ src/styles/Variables.jsx
@@ -0,0 +1,34 @@
+export const maxWidthInt = 1770;
+export const desktopWidthInt = 1100;
+export const tabletWidthInt = 1099;
+export const mobileWidthInt = 499;
+export const tabletWidthInt2 = 1330;
+
+export const Variables = {
+  maxWidth: `${maxWidthInt}px`,
+  desktopW: `${desktopWidthInt}px`,
+  tabletWidth: `${tabletWidthInt}px`,
+  tabletWidth2: `${tabletWidthInt2}px`,
+  mobileWidth: `${mobileWidthInt}px`,
+
+  // colors
+  // When swapping leave note of what the color looks like
+  // used in button link styling
+  color1: "#E5B8F4", // light purple
+  color2: "#C147E9", // purple
+  color3: "#FCF9BE", // light yellow-white
+
+  black: "black",
+  white: "#ffff",
+
+  // Type
+  joseph: "Josefin Sans, sans-serif",
+  openSans: "Open Sans, sans-serif",
+  // Company name
+  companyName: "Digital Delight",
+
+  // Logos
+  logo1Url: "digitaldelight.png",
+
+  background: "/Images/texture_background.jpg",
+};
