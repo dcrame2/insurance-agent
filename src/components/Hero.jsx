9de47c7697962582e4961d1d351ba3bbf2171c62
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-03 20:10:53 -0600

    push

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,17 +3,19 @@ import styled from "styled-components";
 import { H1Styles } from "../styles/Type";
 import { Variables } from "../styles/Variables";
 import { Container, MediaQueries } from "../styles/Utilities";
+import { GlassEffect } from "../styles/Utilities";
 
 const HeroContainer = styled.section`
   ${Container}
   width: 100vw;
   height: 100vh;
   margin-top: 150px;
+
   .hero-inner-container {
     display: flex;
     flex-direction: row;
     align-items: center;
-    /* flex-wrap: wrap; */
+
     gap: 30px;
     @media ${MediaQueries.tablet} {
       flex-direction: column;
@@ -23,8 +25,7 @@ const HeroContainer = styled.section`
     }
     h1 {
       text-transform: uppercase;
-      ${H1Styles}
-      color: ${Variables.cfgreen};
+      ${H1Styles}/* color: ${Variables.black}; */
     }
     h2 {
       text-transform: uppercase;
@@ -44,15 +45,38 @@ const HeroContainer = styled.section`
   }
 `;
 
+const OpaqueFilter = styled.div`
+  background: linear-gradient(
+    90deg,
+    ${Variables.cfgreen} 0%,
+    rgba(125, 125, 125, 0) 90%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  opacity: 0.8;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 80%;
+  z-index: 1;
+
+  @media ${MediaQueries.tablet} {
+    width: 100%;
+
+    ${GlassEffect}
+  }
+`;
+
 const Hero = () => {
   return (
-    <HeroContainer>
-      <div className="hero-inner-container">
-        <h1>Michael Moulis</h1>
-        <img src="agent-nobg.png" alt="Michael Moulis" srcset="" />
-        <h2>Country Financial</h2>
-      </div>
-    </HeroContainer>
+    <OpaqueFilter>
+      <HeroContainer>
+        <div className="hero-inner-container">
+          <h1>Michael Moulis</h1>
+          <img src="agent-nobg.png" alt="Michael Moulis" srcset="" />
+          <h2>Country Financial</h2>
+        </div>
+      </HeroContainer>
+    </OpaqueFilter>
   );
 };
 
