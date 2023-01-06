0f858deebc4e0e2aeefc7e249f608577e33e39f2
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-05 20:23:12 -0600

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,104 +1,121 @@
-import React from 'react';
-import styled from 'styled-components';
-import { H1Styles, PSecondary, H2Styles } from '../styles/Type';
-import { Variables } from '../styles/Variables';
-import { Container, MediaQueries } from '../styles/Utilities';
-import { GlassEffect } from '../styles/Utilities';
+import React from "react";
+import styled from "styled-components";
+import { H1Styles, PSecondary, H2Styles } from "../styles/Type";
+import { Variables } from "../styles/Variables";
+import { Container, MediaQueries } from "../styles/Utilities";
+import { GlassEffect } from "../styles/Utilities";
 
 const HeroContainer = styled.section`
-    ${Container}
+  width: 100vw;
+  height: 100vh;
+
+  ${Container}
+  .hero-inner-container {
     width: 100vw;
     height: 100vh;
-    margin-top: 150px;
+    display: flex;
+    flex-direction: row;
 
-    .hero-inner-container {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        /* justify-content: space-between; */
+    padding-top: 150px;
 
-        gap: 30px;
+    gap: 120px;
+    @media ${MediaQueries.tabletWidthInt2} {
+      /* padding-top: 0; */
+      gap: 0;
+    }
+    @media ${MediaQueries.tablet} {
+      /* flex-direction: column; */
+    }
+    @media ${MediaQueries.mobile} {
+      align-items: left;
+      padding-top: 160px;
+      gap: 0;
+      flex-direction: column;
+    }
+
+    .hero-content {
+      flex-direction: column;
+      display: flex;
+      /* align-items: center; */
+      justify-content: center;
+      gap: 10px;
+      h1 {
+        text-transform: uppercase;
+        ${H1Styles}/* color: ${Variables.black}; */
+      }
+      p {
+        ${PSecondary}
+      }
+
+      h2 {
+        text-transform: uppercase;
+        ${H2Styles}
+        color: ${Variables.white};
+      }
+    }
+
+    .hero-img-content {
+      max-width: 400px;
+      display: flex;
+      flex-direction: column;
+      /* align-items: end; */
+      justify-content: end;
+
+      img {
+        height: 700px;
+        width: auto;
+        /* right: 200px; */
         @media ${MediaQueries.tablet} {
-            flex-direction: column;
+          height: 500px;
         }
         @media ${MediaQueries.mobile} {
-            align-items: left;
+          height: 250px;
         }
-
-        .hero-content {
-            flex-direction: column;
-            display: flex;
-            gap: 10px;
-            h1 {
-                text-transform: uppercase;
-                ${H1Styles}/* color: ${Variables.black}; */
-            }
-            p {
-                ${PSecondary}
-            }
-        }
-
-        h2 {
-            text-transform: uppercase;
-            ${H1Styles}
-            color: ${Variables.white};
-        }
-        img {
-            height: 500px;
-            width: auto;
-            @media ${MediaQueries.tablet} {
-                height: 350px;
-            }
-            @media ${MediaQueries.mobile} {
-                height: 250px;
-            }
-        }
+      }
     }
+  }
 `;
 
 const OpaqueFilter = styled.div`
-    background: linear-gradient(
-        180deg,
-        ${Variables.cfgreen} 0%,
-        rgba(125, 125, 125, 0) 95%,
-        rgba(255, 255, 255, 0) 100%
-    );
-    opacity: 0.8;
-    position: absolute;
-    top: 0;
-    bottom: 0;
+  background: linear-gradient(
+    180deg,
+    ${Variables.cfgreen} 0%,
+    rgba(125, 125, 125, 0) 95%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  opacity: 0.8;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  z-index: 1;
+
+  @media ${MediaQueries.tablet} {
     width: 100%;
-    z-index: 1;
 
-    @media ${MediaQueries.tablet} {
-        width: 100%;
-
-        ${GlassEffect}
-    }
+    ${GlassEffect}
+  }
 `;
 
 const Hero = ({ ...props }) => {
-    return (
-        <OpaqueFilter>
-            <HeroContainer>
-                <div className='hero-inner-container'>
-                    <div className='hero-content'>
-                        <p>{props.eyebrow1}</p>
-                        <h1>{props.heading1}</h1>
-                    </div>
-                    <img
-                        src={props.heroImgSrc}
-                        alt={props.heroImgAlt}
-                        srcset=''
-                    />
-                    <div className='hero-content'>
-                        <p>{props.eyebrow2}</p>
-                        <h2>{props.heading2}</h2>
-                    </div>
-                </div>
-            </HeroContainer>
-        </OpaqueFilter>
-    );
+  return (
+    <OpaqueFilter>
+      <HeroContainer>
+        <div className="hero-inner-container">
+          <div className="hero-content">
+            <p>{props.eyebrow1}</p>
+            <h1>{props.heading1}</h1>
+            <h2>{props.heading2}</h2>
+          </div>
+          <div className="hero-img-content">
+            <div>
+              <img src={props.heroImgSrc} alt={props.heroImgAlt} srcset="" />
+            </div>
+          </div>
+        </div>
+      </HeroContainer>
+    </OpaqueFilter>
+  );
 };
 
 export default Hero;
