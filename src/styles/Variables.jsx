f064646fab589de2f938fbb7ce356b2148afce29
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-07 16:17:05 -0600

    push

--- src/styles/Variables.jsx
+++ src/styles/Variables.jsx
@@ -5,25 +5,26 @@ export const mobileWidthInt = 499;
 export const tabletWidthInt2 = 1330;
 
 export const Variables = {
-    maxWidth: `${maxWidthInt}px`,
-    desktopW: `${desktopWidthInt}px`,
-    tabletWidth: `${tabletWidthInt}px`,
-    tabletWidth2: `${tabletWidthInt2}px`,
-    mobileWidth: `${mobileWidthInt}px`,
+  maxWidth: `${maxWidthInt}px`,
+  desktopW: `${desktopWidthInt}px`,
+  tabletWidth: `${tabletWidthInt}px`,
+  tabletWidth2: `${tabletWidthInt2}px`,
+  mobileWidth: `${mobileWidthInt}px`,
 
-    // colors
-    // When swapping leave note of what the color looks like
-    // used in button link styling
-    cfgreen: '#007a3e', // green
-    color2: '#3d3935', // dark gray
-    black: 'black',
-    white: '#ffff',
+  // colors
+  // When swapping leave note of what the color looks like
+  // used in button link styling
+  cfgreen: "#007a3e", // green
+  // cfgreen: "#ff0000", // state farm testing
+  color2: "#3d3935", // dark gray
+  black: "black",
+  white: "#ffff",
 
-    // Type
-    sourceSans: 'Source Sans Pro, sans-serif',
-    ubuntu: 'Ubuntu, sans-serif',
+  // Type
+  sourceSans: "Source Sans Pro, sans-serif",
+  ubuntu: "Ubuntu, sans-serif",
 
-    companyName: 'Country Financial',
+  companyName: "Country Financial",
 
-    background: '/Images/texture_background.jpg',
+  background: "/Images/texture_background.jpg",
 };
