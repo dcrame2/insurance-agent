4ed4a18f797956b5568fd0be2d175e6c3f56bb3c
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-07 17:10:47 -0600

    push

--- src/App.js
+++ src/App.js
@@ -1,44 +1,55 @@
-import './global.css';
-import { useState } from 'react';
-import Hero from './components/Hero';
-import Navigation from './components/Navigation';
-import InsuranceTypes from './components/InsuranceTypes';
+import "./global.css";
+import { useState } from "react";
+import Hero from "./components/Hero";
+import Navigation from "./components/Navigation";
+import InsuranceTypes from "./components/InsuranceTypes";
 import {
-    HomeData,
-    NavigationData,
-    InsuranceTypesData,
-    FaqData,
-} from './data/Data';
-import FaqModule from './components/FaqModule';
-import Spacer from './sub_components/Spacer';
-import { Variables } from './styles/Variables';
+  HomeData,
+  NavigationData,
+  InsuranceTypesData,
+  FaqData,
+} from "./data/Data";
+import FaqModule from "./components/FaqModule";
+import Spacer from "./sub_components/Spacer";
+import { Variables } from "./styles/Variables";
 
 function App() {
-    const [navBackdrop, setNavBackdrop] = useState(false);
-    const [scrollY, setScrollY] = useState();
+  const [navBackdrop, setNavBackdrop] = useState(false);
+  const [scrollY, setScrollY] = useState();
 
-    const scrollListener = () => {
-        window.onscroll = function (e) {
-            setScrollY(this.scrollY);
-            // Nav functionality
-            if (this.scrollY > 83) {
-                setNavBackdrop(true);
-            } else {
-                setNavBackdrop(false);
-            }
-        };
+  const scrollListener = () => {
+    window.onscroll = function (e) {
+      setScrollY(this.scrollY);
+      // Nav functionality
+      if (this.scrollY > 83) {
+        setNavBackdrop(true);
+      } else {
+        setNavBackdrop(false);
+      }
     };
+  };
 
-    scrollListener();
-    return (
-        <>
-            <Navigation backdrop={navBackdrop} data={NavigationData} />
-            <Hero data={HomeData} />
-            <InsuranceTypes data={InsuranceTypesData} />
-
-            <FaqModule data={FaqData} />
-        </>
-    );
+  scrollListener();
+  return (
+    <>
+      <Navigation backdrop={navBackdrop} data={NavigationData} />
+      <Hero data={HomeData} />
+      <Spacer
+        color={Variables.color20}
+        desktop={244}
+        tablet={120}
+        mobile={100}
+      />
+      <InsuranceTypes data={InsuranceTypesData} />
+      <Spacer
+        color={Variables.color20}
+        desktop={244}
+        tablet={120}
+        mobile={100}
+      />
+      <FaqModule data={FaqData} />
+    </>
+  );
 }
 
 export default App;
