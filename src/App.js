710c0cb019a650520a38b796ca9f5dd9370ca54e
Joshua Tully <jtully97@gmail.com>
2023-01-15 22:45:07 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './global.css';
-import { useState } from 'react';
-import Hero from './components/Hero';
+import { useState, useEffect } from 'react';
 import Navigation from './components/Navigation';
-import InsuranceTypes from './components/InsuranceTypes';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import {
-    HomeData,
     NavigationData,
-    InsuranceTypesData,
-    FaqData,
     ContactFormData,
     FooterData,
     HomeInsuranceICMData,
     HomeInsuranceFaqData,
 } from './data/Data';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import BusinessInsurance from './pages/BusinessInsurance';
 import HomeInsurance from './pages/HomeInsurance';
@@ -23,7 +18,7 @@ import LifeInsurance from './pages/LifeInsurance';
 import CarInsurance from './pages/CarInsurance';
 import ScrollToTop from './components/ScrollToTop';
 
-function App() {
+export default function App() {
     const [navBackdrop, setNavBackdrop] = useState(false);
     const [scrollY, setScrollY] = useState();
 
@@ -43,36 +38,57 @@ function App() {
 
     return (
         <>
-            <Router>
+            <BrowserRouter>
                 <ScrollToTop />
                 <Navigation backdrop={navBackdrop} data={NavigationData} />
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/car-insurance' element={<CarInsurance />} />
-                    <Route path='/life-insurance' element={<LifeInsurance />} />
-                    <Route
-                        path='/home-insurance'
-                        element={
-                            <HomeInsurance
-                                data={
-                                    (HomeInsuranceICMData, HomeInsuranceFaqData)
-                                }
-                            />
-                        }
-                    />
-                    <Route
-                        path='/business-insurance'
-                        element={<BusinessInsurance />}
-                    />
-                    <Route
-                        path='/contact'
-                        element={<Contact data={ContactFormData} />}
-                    />
-                </Routes>
+                <Content />
                 <Footer data={FooterData} />
-            </Router>
+            </BrowserRouter>
         </>
     );
 }
 
-export default App;
+function Content() {
+    const location = useLocation();
+
+    const [displayLocation, setDisplayLocation] = useState(location);
+    const [transitionStage, setTransistionStage] = useState('fadeIn');
+
+    useEffect(() => {
+        if (location !== displayLocation) setTransistionStage('fadeOut');
+    }, [location, displayLocation]);
+
+    return (
+        <div
+            className={`${transitionStage}`}
+            onAnimationEnd={() => {
+                if (transitionStage === 'fadeOut') {
+                    setTransistionStage('fadeIn');
+                    setDisplayLocation(location);
+                }
+            }}
+        >
+            <Routes location={displayLocation}>
+                <Route path='/' element={<Home />} />
+                <Route path='/car-insurance' element={<CarInsurance />} />
+                <Route path='/life-insurance' element={<LifeInsurance />} />
+                <Route
+                    path='/home-insurance'
+                    element={
+                        <HomeInsurance
+                            data={(HomeInsuranceICMData, HomeInsuranceFaqData)}
+                        />
+                    }
+                />
+                <Route
+                    path='/business-insurance'
+                    element={<BusinessInsurance />}
+                />
+                <Route
+                    path='/contact'
+                    element={<Contact data={ContactFormData} />}
+                />
+            </Routes>
+        </div>
+    );
+}
