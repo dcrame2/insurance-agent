3c4ac76995a8373937859e506d64453e76b4347d
dcrame2 <36011387+dcrame2@users.noreply.github.com>
2023-01-28 14:00:09 -0600

    fixing contact array

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -226,7 +226,8 @@ const Contact = ({ data }) => {
   };
 
   const recipients = [
-    "thedigitaldelightllc@gmail.com, mike.moulis@countryfinanical.com",
+    "thedigitaldelightllc@gmail.com",
+    "mike.moulis@countryfinanical.com",
   ];
 
   const submitForm = (recip) => {
