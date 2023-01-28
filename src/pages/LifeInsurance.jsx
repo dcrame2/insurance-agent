5407bd9b1638925e1a7a59e7842f3c5b71f9eadc
Joshua Tully <jtully97@gmail.com>
2023-01-28 13:52:15 -0600

    update spacer

--- src/pages/LifeInsurance.jsx
+++ src/pages/LifeInsurance.jsx
@@ -1,49 +1,68 @@
-import React from "react";
-import { Variables } from "../styles/Variables";
+import React from 'react';
+import { Variables } from '../styles/Variables';
 import {
-  LifeInsuranceFaqData,
-  LifeInsuranceICMData,
-  ContactBarData,
-  LifeInsuranceContentModuleData,
-  LifeFillerModuleData,
-} from "../data/Data";
-import ImageContentModule from "../components/ImageContentModule";
-import Spacer from "../sub_components/Spacer";
-import FaqModule from "../components/FaqModule";
-import ContactBarModule from "../components/ContactBarModule";
-import ContentModule from "../components/ContentModule";
-import FillerModule from "../components/FillerModule";
+    LifeInsuranceFaqData,
+    LifeInsuranceICMData,
+    ContactBarData,
+    LifeInsuranceContentModuleData,
+    LifeFillerModuleData,
+} from '../data/Data';
+import ImageContentModule from '../components/ImageContentModule';
+import Spacer from '../sub_components/Spacer';
+import FaqModule from '../components/FaqModule';
+import ContactBarModule from '../components/ContactBarModule';
+import ContentModule from '../components/ContentModule';
+import FillerModule from '../components/FillerModule';
 
 const LifeInsurance = () => {
-  return (
-    <>
-      <ImageContentModule imgPlacement="right" {...LifeInsuranceICMData} />
-      <Spacer
-        color={Variables.primaryColor2}
-        desktop={100}
-        tablet={80}
-        mobile={40}
-      />
-      <ContentModule data={LifeInsuranceContentModuleData} columns={2} />
-      <Spacer color={Variables.black} desktop={100} tablet={80} mobile={40} />
-      <FillerModule data={LifeFillerModuleData} />
-      <Spacer color={Variables.black} desktop={100} tablet={80} mobile={40} />
-      <Spacer
-        color={Variables.primaryColor2}
-        desktop={100}
-        tablet={80}
-        mobile={40}
-      />
-      <FaqModule {...LifeInsuranceFaqData} />
-      <Spacer
-        color={Variables.primaryColor2}
-        desktop={100}
-        tablet={80}
-        mobile={40}
-      />
-      <ContactBarModule data={ContactBarData} />
-    </>
-  );
+    return (
+        <>
+            <ImageContentModule
+                imgPlacement='right'
+                {...LifeInsuranceICMData}
+            />
+            <Spacer
+                color={Variables.primaryColor2}
+                desktop={100}
+                tablet={80}
+                mobile={40}
+            />
+            <ContentModule data={LifeInsuranceContentModuleData} columns={2} />
+            <Spacer
+                color={Variables.primaryColor2}
+                desktop={100}
+                tablet={80}
+                mobile={40}
+            />
+            <Spacer
+                color={Variables.black}
+                desktop={100}
+                tablet={80}
+                mobile={40}
+            />
+            <FillerModule data={LifeFillerModuleData} />
+            <Spacer
+                color={Variables.black}
+                desktop={100}
+                tablet={80}
+                mobile={40}
+            />
+            <Spacer
+                color={Variables.primaryColor2}
+                desktop={100}
+                tablet={80}
+                mobile={40}
+            />
+            <FaqModule {...LifeInsuranceFaqData} />
+            <Spacer
+                color={Variables.primaryColor2}
+                desktop={100}
+                tablet={80}
+                mobile={40}
+            />
+            <ContactBarModule data={ContactBarData} />
+        </>
+    );
 };
 
 export default LifeInsurance;
