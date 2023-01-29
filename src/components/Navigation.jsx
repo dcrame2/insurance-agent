6905188d7f933cfba0771533303068932342cb4f
Joshua Tully <jtully97@gmail.com>
2023-01-28 22:16:28 -0600

    update navigation link with framer

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -284,7 +284,7 @@ export default function Navigation({ backdrop, data }) {
                                                     setHamburgerPressed(false)
                                                 }
                                                 key={`textLink${index}`}
-                                                whileHover={{ scale: 1.2 }}
+                                                whileHover={{ scale: 1.08 }}
                                                 whileTap={{ scale: 0.9 }}
                                                 transition={{
                                                     type: 'spring',
