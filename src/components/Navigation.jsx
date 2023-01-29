7964fe668b21123da3203d575e7cbb96da6435c7
Joshua Tully <jtully97@gmail.com>
2023-01-28 21:31:37 -0600

    update ham button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -314,7 +314,7 @@ export default function Navigation({ backdrop, data }) {
                             aria-label='toggle mobile menu'
                             onClick={HamburgerToggle}
                             whileHover={{ scale: 1.08 }}
-                            whileTap={{ scale: 0.85 }}
+                            whileTap={{ scale: 0.8 }}
                             transition={{
                                 type: 'spring',
                                 stiffness: 400,
